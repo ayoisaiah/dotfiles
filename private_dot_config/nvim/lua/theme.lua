27f714fd3c9ca54eb0a1217db46f330156b8bf80
Ayooluwa Isaiah <ayisaiah@gmail.com>
2021-08-22 13:13:13 +0100

    Improve selection colour

--- private_dot_config/nvim/lua/theme.lua
+++ private_dot_config/nvim/lua/theme.lua
@@ -2,7 +2,7 @@ local present, base16 = pcall(require, 'base16')
 
 if present then
   local hardcore = base16.theme_from_array {
-    "1b1f27"; "1e222a"; "252931"; "4A4A4A";
+    "1b1f27"; "1e222a"; "30343c"; "4A4A4A";
     "42464e"; "cdcdcd"; "e5e5e5"; "ffffff";
     "fb4934"; "fd971f"; "e6db74"; "a6e22e";
     "708387"; "66d9ef"; "9e6ffe"; "e8b882";
