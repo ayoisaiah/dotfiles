e8dd103a8279dc623c81aaefe9fadf32afd594a1
Ayooluwa Isaiah <ayisaiah@gmail.com>
2021-12-07 19:59:52 +0100

    Add config for lua and prettier

--- private_dot_config/nvim/lua/plugins/formatter-nvim.lua
+++ private_dot_config/nvim/lua/plugins/formatter-nvim.lua
@@ -1,27 +1,40 @@
-require('formatter').setup({
-  filetype = {
-    javascript = {
-      function()
-        return {
-          exe = "prettier",
-          stdin = true
-        }
-      end
-    },
-    go = {
-      function()
-        return {
-          exe = "goimports",
-          stdin = true
-        }
-      end
-    }
-  }
+require("formatter").setup({
+	filetype = {
+		javascript = {
+			function()
+				return {
+					exe = "prettier",
+					args = "--config",
+					"$HOME/.prettierrc.json",
+					stdin = true,
+				}
+			end,
+		},
+		go = {
+			function()
+				return {
+					exe = "goimports",
+					stdin = true,
+				}
+			end,
+		},
+		lua = {
+			function()
+				return {
+					exe = "stylua",
+					stdin = true,
+				}
+			end,
+		},
+	},
 })
 
-vim.api.nvim_exec([[
+vim.api.nvim_exec(
+	[[
 augroup FormatAutogroup
   autocmd!
   autocmd BufWritePost *.js,*.ts,*.go FormatWrite
 augroup END
-]], true)
+]],
+	true
+)
