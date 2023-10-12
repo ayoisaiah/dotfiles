b74e7c0856400bca462b839e6a04f7f2f06ee60c
Ayooluwa Isaiah <ayisaiah@gmail.com>
2023-10-12 18:50:32 +0200

    replace none-ls with nvim-lint

---
+++ private_dot_config/nvim/lua/plugins/nvim-lint.lua
@@ -0,0 +1,24 @@
+local config = function()
+	local lint = require("lint")
+	lint.linters_by_ft = {
+		markdown = { "markdownlint" },
+		sh = { "shellcheck" },
+		dockerfile = { "hadolint" },
+		javascript = { "eslint" },
+		json = { "jsonlint" },
+		go = { "golangcilint" },
+		ruby = { "rubocop" },
+		eruby = { "erb_lint" },
+		css = { "stylelint" },
+		scss = { "stylelint" },
+		sql = { "sqlfluff" },
+		yaml = { "actionlint", "yamllint" },
+		["yaml.ansible"] = { "ansiblelint" },
+	}
+end
+
+return {
+	"mfussenegger/nvim-lint",
+	event = { "BufReadPost" },
+	config,
+}
